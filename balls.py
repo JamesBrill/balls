bb4589157743ee3ac6dc8d3dcf5912049ab0cccd
James Brill <contact@jamesbrill.co.uk>
2017-07-08 16:58:20 +0100

    Add gravity

--- balls.py
+++ balls.py
@@ -8,8 +8,9 @@ import math
 pygame.init()
 WINDOWWIDTH = 400
 WINDOWHEIGHT = 400
-NUMBER_OF_BALLS = 100
+NUMBER_OF_BALLS = 1
 BLACK = (0, 0, 0)
+GRAVITY = (math.pi, 0.5)
 windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 pygame.display.set_caption('Balls')
 
@@ -25,6 +26,14 @@ for i in range(NUMBER_OF_BALLS):
     }
     balls.append(ball)
 
+
+def add_vectors(angle1, length1, angle2, length2):
+    x = math.sin(angle1) * length1 + math.sin(angle2) * length2
+    y = math.cos(angle1) * length1 + math.cos(angle2) * length2
+    length = math.hypot(x, y)
+    angle = 0.5 * math.pi - math.atan2(y, x)
+    return (angle, length)
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -39,10 +48,13 @@ while True:
 
         if ball['x'] - ball['radius'] < 0 or ball['x'] + ball['radius'] > WINDOWWIDTH:
             ball['angle'] = 2 * math.pi - ball['angle']
+            ball['x'] = min(max(ball['x'], ball['radius']), WINDOWWIDTH - ball['radius'])
 
         if ball['y'] - ball['radius'] < 0 or ball['y'] + ball['radius'] > WINDOWHEIGHT:
             ball['angle'] = math.pi - ball['angle']
+            ball['y'] = min(max(ball['y'], ball['radius']), WINDOWHEIGHT - ball['radius'])
 
+        (ball['angle'], ball['speed']) = add_vectors(ball['angle'], ball['speed'], GRAVITY[0], GRAVITY[1])
         pygame.draw.circle(windowSurface, ball['color'], (int(ball['x']), int(ball['y'])), ball['radius'])
 
     pygame.display.update()
